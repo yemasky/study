90fdd34d9887935fb82c8b1bfddb4c5c007666a6
yemasky <yemasky@msn.com>
2023-12-14 11:12:46 +0800

    layui

---
+++ SpringCloudAlibaba/cloud_layui/moyu-modules/moyu-system/src/main/java/cn/xueden/system/dao/LogDao.java
@@ -0,0 +1,15 @@
+package cn.xueden.system.dao;
+
+
+import cn.xueden.common.core.web.domain.SysLog;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**功能描述:系统日志 Mapper 接口
+ * @Auther:http://www.xueden.cn
+ * @Date:2020/3/6
+ * @Description:cn.xueden.modules.system.dao
+ * @version:1.0
+ */
+public interface LogDao extends BaseMapper<SysLog> {
+}
